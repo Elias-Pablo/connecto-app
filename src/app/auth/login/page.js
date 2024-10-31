848cc9fd5662d9cf5865477aadd447d1863fe06c
Cockfla <eliasbaha100@gmail.com>
2024-10-31 17:31:08 -0300

    push

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,14 +1,11 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { jwtDecode } from "jwt-decode"; // Importar decodificador de JWT
+import { jwtDecode } from "jwt-decode"; // Importación corregida
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false); // Estado para controlar visibilidad
   const [error, setError] = useState(null);
   const router = useRouter();
 
@@ -38,8 +35,6 @@ export default function LoginPage() {
         } else {
           router.push("/"); // Página para usuarios regulares
         }
-
-        router.refresh();
       } else {
         setError(data.error);
       }
@@ -50,22 +45,11 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-neutral-900 p-5">
-      <Link href="/">
-        <Image
-          src="/ConnecTo-logo.png"
-          alt="ConnecTo Logo"
-          width={250}
-          height={250}
-          className="mb-10"
-        />
-      </Link>
+      <h1 className="text-2xl font-bold text-white mb-6">Iniciar Sesión</h1>
       <form
         onSubmit={handleLogin}
         className="bg-fuchsia-600 w-full max-w-md p-8 rounded-lg shadow-lg"
       >
-        <h1 className="text-2xl text-center font-bold text-white mb-6">
-          Iniciar Sesión
-        </h1>
         <div className="mb-4">
           <label htmlFor="email" className="block text-white mb-2">
             Correo Electrónico
@@ -80,12 +64,12 @@ export default function LoginPage() {
             required
           />
         </div>
-        <div className="mb-6 relative ">
+        <div className="mb-6">
           <label htmlFor="password" className="block text-white mb-2">
             Contraseña
           </label>
           <input
-            type={showPassword ? "text" : "password"} // Cambia el tipo de input
+            type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -93,48 +77,6 @@ export default function LoginPage() {
             placeholder="********"
             required
           />
-          <button
-            type="button"
-            onClick={() => setShowPassword(!showPassword)} // Alterna visibilidad
-            className="absolute right-3 top-10"
-          >
-            {showPassword ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-eye-off"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
-                <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
-                <path d="M3 3l18 18" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-eye"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-              </svg>
-            )}
-          </button>
         </div>
         <button
           type="submit"
