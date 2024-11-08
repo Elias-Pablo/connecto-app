bd99d315ea1bfe1255a174a328ead726ee1b7047
Cockfla <eliasbaha100@gmail.com>
2024-11-08 20:55:29 -0300

    ahora si?

--- src/app/api/user/emprendedores/profile/[id]/route.js
+++ src/app/api/user/emprendedores/profile/[id]/route.js
@@ -1,4 +1,5 @@
 import connection from "@/lib/db";
+export const dynamic = "force-dynamic"; // Forzar la generación dinámica para esta ruta
 
 // Obtener datos completos del emprendedor por ID de perfil
 export async function GET(req, { params }) {
@@ -8,50 +9,22 @@ export async function GET(req, { params }) {
     if (!profileId) {
       return new Response(
         JSON.stringify({ message: "ID de perfil es requerido" }),
-        { status: 400 }
+        { status: 400, headers: { "Content-Type": "application/json" } }
       );
     }
 
-    const emprendedorData = await new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT e.nombre_negocio, e.descripcion, e.direccion, e.telefono, e.sitioweb_url, i.url_imagen
-         FROM perfil_negocio e
-         LEFT JOIN imagen_publicacion i ON e.id_imagen = i.id_imagen
-         WHERE e.id_perfil = ?`,
-        [profileId],
-        (error, results) => {
-          if (error) {
-            console.error("Error al obtener datos del emprendedor:", error);
-            return reject(new Error("Error al obtener datos del emprendedor"));
-          }
-          resolve(results[0] || null);
-        }
-      );
-    });
-
-    const productos = await new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT p.id_producto, p.nombre AS producto_nombre, p.descripcion, p.precio, i.url_imagen
-         FROM productos p
-         LEFT JOIN imagen_publicacion i ON p.id_imagen = i.id_imagen
-         WHERE p.id_perfil = ?`,
-        [profileId],
-        (error, results) => {
-          if (error) {
-            console.error("Error al obtener productos:", error);
-            return reject(new Error("Error al obtener productos"));
-          }
-          resolve(results);
-        }
-      );
-    });
+    // Obtener datos del emprendedor
+    const emprendedorData = await obtenerDatosEmprendedor(profileId);
 
     if (!emprendedorData) {
       return new Response(
         JSON.stringify({ message: "Emprendedor no encontrado" }),
-        { status: 404 }
+        { status: 404, headers: { "Content-Type": "application/json" } }
       );
     }
+
+    // Obtener productos del emprendedor
+    const productos = await obtenerProductos(profileId);
 
     return new Response(JSON.stringify({ emprendedorData, productos }), {
       status: 200,
@@ -61,7 +34,49 @@ export async function GET(req, { params }) {
     console.error("Error al obtener datos del emprendedor:", error);
     return new Response(
       JSON.stringify({ message: "Error al obtener datos del emprendedor" }),
-      { status: 500 }
+      { status: 500, headers: { "Content-Type": "application/json" } }
     );
   }
+}
+
+// Función para obtener los datos del emprendedor
+async function obtenerDatosEmprendedor(profileId) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT e.nombre_negocio, e.descripcion, e.direccion, e.telefono, e.sitioweb_url, i.url_imagen
+       FROM perfil_negocio e
+       LEFT JOIN imagen_publicacion i ON e.id_imagen = i.id_imagen
+       WHERE e.id_perfil = ?`,
+      [profileId],
+      (error, results) => {
+        if (error) {
+          console.error("Error al obtener datos del emprendedor:", error);
+          reject(new Error("Error al obtener datos del emprendedor"));
+        } else {
+          resolve(results[0] || null);
+        }
+      }
+    );
+  });
+}
+
+// Función para obtener los productos del emprendedor
+async function obtenerProductos(profileId) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT p.id_producto, p.nombre AS producto_nombre, p.descripcion, p.precio, i.url_imagen
+       FROM productos p
+       LEFT JOIN imagen_publicacion i ON p.id_imagen = i.id_imagen
+       WHERE p.id_perfil = ?`,
+      [profileId],
+      (error, results) => {
+        if (error) {
+          console.error("Error al obtener productos:", error);
+          reject(new Error("Error al obtener productos"));
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
 }
--- src/app/user/emprendedores/profile/page.js
+++ src/app/user/emprendedores/profile/page.js
@@ -33,7 +33,7 @@ export default function EmprendedorProfile() {
         );
         if (response.ok) {
           const data = await response.json();
-          setEmprendedorData(data.emprendedorData || null);
+          setEmprendedorData(data.emprendedorData || {});
           setProductos(data.productos || []);
           setResenas(data.resenas || []);
         } else {
@@ -47,122 +47,119 @@ export default function EmprendedorProfile() {
     fetchProfileData();
   }, [idPerfil]);
 
-  if (!emprendedorData) {
-    return (
-      <p className="text-center text-gray-500">
-        No se encontró información del emprendedor o ocurrió un error al cargar
-        los datos.
-      </p>
-    );
-  }
-
   return (
     <CartProvider>
       <Header />
       <Suspense
         fallback={<p className="text-center text-gray-500">Cargando...</p>}
       >
-        <section className="p-10">
-          <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-            <img
-              src={emprendedorData.url_imagen || "/placeholder.webp"}
-              alt={emprendedorData.nombre_negocio || "Emprendedor"}
-              width={150}
-              height={150}
-              className="mx-auto rounded-full mb-4"
-            />
-            <h1 className="text-2xl font-bold mb-2 text-black">
-              {emprendedorData.nombre_negocio}
-            </h1>
-            <p className="text-sm mb-2 text-black">
-              Dirección: {emprendedorData.direccion || "No disponible"}
-            </p>
-            <p className="text-sm mb-2 text-black">
-              Teléfono: {emprendedorData.telefono || "No disponible"}
-            </p>
-            {emprendedorData.sitio_web ? (
-              <a
-                href={emprendedorData.sitio_web}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                {emprendedorData.sitio_web}
-              </a>
-            ) : (
-              <p className="text-sm text-gray-500">Sitio web no disponible</p>
-            )}
-          </div>
-
-          <div className="mt-10">
-            <h2 className="text-xl font-semibold mb-4">Productos</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {productos.length > 0 ? (
-                productos.map((producto) => (
-                  <div
-                    key={producto.id_producto}
-                    className="bg-gray-100 p-4 rounded-lg shadow-md flex flex-col items-center justify-around"
-                  >
-                    <img
-                      src={producto.url_imagen || "/placeholder.jpg"}
-                      alt={producto.producto_nombre}
-                      width={200}
-                      height={150}
-                      className="rounded-lg mb-2"
-                    />
-                    <h3 className="text-lg font-bold text-black">
-                      {producto.producto_nombre}
-                    </h3>
-                    <p className="text-sm mb-2 text-black">
-                      {producto.descripcion}
-                    </p>
-                    <p className="text-blue-500 font-semibold">
-                      {formatPrice(producto.precio)}
-                    </p>
-                    <button
-                      onClick={() => addToCart(producto)}
-                      className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                    >
-                      Agregar al Carrito
-                    </button>
-                  </div>
-                ))
+        {emprendedorData ? (
+          <section className="p-10">
+            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+              <img
+                src={emprendedorData.url_imagen || "/placeholder.webp"}
+                alt={emprendedorData.nombre_negocio || "Emprendedor"}
+                width={150}
+                height={150}
+                className="mx-auto rounded-full mb-4"
+              />
+              <h1 className="text-2xl font-bold mb-2 text-black">
+                {emprendedorData.nombre_negocio}
+              </h1>
+              <p className="text-sm mb-2 text-black">
+                Dirección: {emprendedorData.direccion || "No disponible"}
+              </p>
+              <p className="text-sm mb-2 text-black">
+                Teléfono: {emprendedorData.telefono || "No disponible"}
+              </p>
+              {emprendedorData.sitio_web ? (
+                <a
+                  href={emprendedorData.sitio_web}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline"
+                >
+                  {emprendedorData.sitio_web}
+                </a>
               ) : (
-                <p className="text-center text-gray-500">
-                  No hay productos disponibles.
+                <p className="text-sm text-gray-500">Sitio web no disponible</p>
+              )}
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-xl font-semibold mb-4">Productos</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {productos.length > 0 ? (
+                  productos.map((producto) => (
+                    <div
+                      key={producto.id_producto}
+                      className="bg-gray-100 p-4 rounded-lg shadow-md flex flex-col items-center justify-around"
+                    >
+                      <img
+                        src={producto.url_imagen || "/placeholder.jpg"}
+                        alt={producto.producto_nombre}
+                        width={200}
+                        height={150}
+                        className="rounded-lg mb-2"
+                      />
+                      <h3 className="text-lg font-bold text-black">
+                        {producto.producto_nombre}
+                      </h3>
+                      <p className="text-sm mb-2 text-black">
+                        {producto.descripcion}
+                      </p>
+                      <p className="text-blue-500 font-semibold">
+                        {formatPrice(producto.precio)}
+                      </p>
+                      <button
+                        onClick={() => addToCart(producto)}
+                        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                      >
+                        Agregar al Carrito
+                      </button>
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-center text-gray-500">
+                    No hay productos disponibles.
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-xl font-semibold mb-4">Reseñas</h2>
+              {resenas.length > 0 ? (
+                <div className="space-y-4">
+                  {resenas.map((resena, index) => (
+                    <div
+                      key={index}
+                      className="bg-white p-4 rounded-lg shadow-md"
+                    >
+                      <p className="text-sm mb-1">
+                        Comentario: {resena.comentario}
+                      </p>
+                      <p className="text-sm text-yellow-500">
+                        Calificación: {resena.calificacion} / 5
+                      </p>
+                      <p className="text-xs text-gray-500">
+                        Por: {resena.usuario}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p className="text-sm text-gray-500">
+                  No hay reseñas disponibles.
                 </p>
               )}
             </div>
-          </div>
-
-          <div className="mt-10">
-            <h2 className="text-xl font-semibold mb-4">Reseñas</h2>
-            {resenas.length > 0 ? (
-              <div className="space-y-4">
-                {resenas.map((resena, index) => (
-                  <div
-                    key={index}
-                    className="bg-white p-4 rounded-lg shadow-md"
-                  >
-                    <p className="text-sm mb-1">
-                      Comentario: {resena.comentario}
-                    </p>
-                    <p className="text-sm text-yellow-500">
-                      Calificación: {resena.calificacion} / 5
-                    </p>
-                    <p className="text-xs text-gray-500">
-                      Por: {resena.usuario}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p className="text-sm text-gray-500">
-                No hay reseñas disponibles.
-              </p>
-            )}
-          </div>
-        </section>
+          </section>
+        ) : (
+          <p className="text-center text-gray-500">
+            No se encontró información del emprendedor.
+          </p>
+        )}
       </Suspense>
     </CartProvider>
   );
