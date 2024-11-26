617825cde6ce5578a7de8ecdc8bea32f17c1e060
Cockfla <eliasbaha100@gmail.com>
2024-11-26 18:02:05 -0300

    xd

--- src/app/user/checkout/page.js
+++ src/app/user/checkout/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import { useCart } from "@/app/context/CartContext";
+import { CartProvider, useCart } from "@/app/context/CartContext";
 
 export default function Checkout() {
   const { cartItems, calculateTotal } = useCart();
@@ -33,26 +33,30 @@ export default function Checkout() {
   };
 
   return (
-    <div className="container mx-auto p-10">
-      <h1 className="text-2xl font-bold mb-4">Página de Pago</h1>
-      <ul className="mb-4">
-        {cartItems.map((item) => (
-          <li key={item.id} className="mb-2">
-            {item.name} - ${item.price} x {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <div className="mt-4">
-        <p className="text-xl font-semibold">
-          Total a pagar: ${calculateTotal().toFixed(2)}
-        </p>
-      </div>
-      <button
-        onClick={handleConfirmPurchase}
-        className="mt-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-      >
-        Confirmar Compra
-      </button>
-    </div>
+    <>
+      <CartProvider>
+        <div className="container mx-auto p-10">
+          <h1 className="text-2xl font-bold mb-4">Página de Pago</h1>
+          <ul className="mb-4">
+            {cartItems.map((item) => (
+              <li key={item.id} className="mb-2">
+                {item.name} - ${item.price} x {item.quantity}
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <p className="text-xl font-semibold">
+              Total a pagar: ${calculateTotal().toFixed(2)}
+            </p>
+          </div>
+          <button
+            onClick={handleConfirmPurchase}
+            className="mt-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+          >
+            Confirmar Compra
+          </button>
+        </div>
+      </CartProvider>
+    </>
   );
 }
--- src/app/user/secure/payment/page.js
+++ src/app/user/secure/payment/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useCart } from "@/app/context/CartContext";
+import { CartProvider, useCart } from "@/app/context/CartContext";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -39,36 +39,40 @@ export default function Checkout() {
   };
 
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold mb-5">Revisar Compra</h1>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id} className="mb-4 flex justify-between">
-            <span>{item.name}</span>
-            <span>
-              {item.quantity} x ${item.price.toFixed(2)}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <div className="mt-6">
-        <div className="flex justify-between">
-          <p>Subtotal:</p>
-          <p>${calculateSubtotal().toFixed(2)}</p>
+    <>
+      <CartProvider>
+        <div className="p-10">
+          <h1 className="text-2xl font-bold mb-5">Revisar Compra</h1>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id} className="mb-4 flex justify-between">
+                <span>{item.name}</span>
+                <span>
+                  {item.quantity} x ${item.price.toFixed(2)}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-6">
+            <div className="flex justify-between">
+              <p>Subtotal:</p>
+              <p>${calculateSubtotal().toFixed(2)}</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p>Total:</p>
+              <p>${calculateTotal().toFixed(2)}</p>
+            </div>
+          </div>
+          {error && <p className="text-red-500 mt-4">{error}</p>}
+          <button
+            onClick={handlePayment}
+            className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-lg"
+            disabled={isLoading}
+          >
+            {isLoading ? "Procesando..." : "Proceder al Pago"}
+          </button>
         </div>
-        <div className="flex justify-between mt-2">
-          <p>Total:</p>
-          <p>${calculateTotal().toFixed(2)}</p>
-        </div>
-      </div>
-      {error && <p className="text-red-500 mt-4">{error}</p>}
-      <button
-        onClick={handlePayment}
-        className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-lg"
-        disabled={isLoading}
-      >
-        {isLoading ? "Procesando..." : "Proceder al Pago"}
-      </button>
-    </div>
+      </CartProvider>
+    </>
   );
 }
