e6d65798f14a9b924ffe4c122b2d059811dedf74
Cockfla <eliasbaha100@gmail.com>
2024-11-29 22:33:01 -0300

    cambios bbdd

--- migraciones/connecto (6).sql
+++ migraciones/connecto (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 26-11-2024 a las 03:38:48
+-- Tiempo de generación: 30-11-2024 a las 02:32:38
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
@@ -58,6 +58,15 @@ CREATE TABLE `compra` (
   `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`id_compra`, `id_documento`, `id_envio`, `id_usuario`, `detalle`, `cantidad`, `estado`, `total`, `fecha_creacion`) VALUES
+(1, 5, 5, NULL, 'Compra de productos', 1, 1, 2000, '2024-11-30 01:18:23'),
+(2, 6, 6, NULL, 'Compra de productos', 1, 1, 2000, '2024-11-30 01:18:59'),
+(3, 7, 7, 11, 'Compra de productos', 1, 1, 2000, '2024-11-30 01:24:26');
+
 -- --------------------------------------------------------
 
 --
@@ -77,7 +86,12 @@ CREATE TABLE `conversaciones` (
 --
 
 INSERT INTO `conversaciones` (`id_conversacion`, `nombre_conversacion`, `id_usuario1`, `id_usuario2`, `fecha_creacion`) VALUES
-(1, 'Chat entre 7 y 9', 7, 9, '2024-11-26 01:21:51');
+(1, 'Chat entre 7 y 9', 7, 9, '2024-11-26 01:21:51'),
+(2, 'Chat entre 7 y 11', 7, 11, '2024-11-26 19:31:42'),
+(3, 'Chat entre 9 y 9', 9, 9, '2024-11-26 19:52:09'),
+(4, 'Chat entre 7 y 10', 7, 10, '2024-11-26 20:22:47'),
+(5, 'Chat entre 11 y 9', 11, 9, '2024-11-29 22:41:29'),
+(6, 'Chat entre 11 y 11', 11, 11, '2024-11-30 00:05:12');
 
 -- --------------------------------------------------------
 
@@ -96,6 +110,15 @@ CREATE TABLE `detalle_compra` (
   `total_con_iva` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `detalle_compra`
+--
+
+INSERT INTO `detalle_compra` (`id_detalle`, `id_compra`, `cantidad`, `precio_unitario`, `subtotal`, `total_sin_iva`, `iva`, `total_con_iva`) VALUES
+(1, 1, 1, '2000.00', '2000.00', '2000.00', '380.00', '2380.00'),
+(2, 2, 1, '2000.00', '2000.00', '2000.00', '380.00', '2380.00'),
+(3, 3, 1, '2000.00', '2000.00', '2000.00', '380.00', '2380.00');
+
 -- --------------------------------------------------------
 
 --
@@ -106,6 +129,34 @@ CREATE TABLE `documento` (
   `id_documento` int(11) NOT NULL,
   `tipo_documento` enum('Boleta,Factura') DEFAULT NULL,
   `tipo_pago` enum('Debito,Credito') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `documento`
+--
+
+INSERT INTO `documento` (`id_documento`, `tipo_documento`, `tipo_pago`) VALUES
+(1, NULL, NULL),
+(2, NULL, NULL),
+(3, NULL, NULL),
+(4, NULL, NULL),
+(5, NULL, NULL),
+(6, NULL, NULL),
+(7, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrepreneur_reviews`
+--
+
+CREATE TABLE `entrepreneur_reviews` (
+  `id_review` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_emprendedor` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `calificacion` int(11) NOT NULL CHECK (`calificacion` between 1 and 5),
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -124,6 +175,40 @@ CREATE TABLE `envio` (
   `tiempo_llegada` datetime DEFAULT NULL,
   `fecha_creacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `envio`
+--
+
+INSERT INTO `envio` (`id_envio`, `direccion`, `destinatario`, `remitente`, `tiempo_estimado`, `valor`, `tiempo_llegada`, `fecha_creacion`) VALUES
+(1, NULL, NULL, NULL, '2024-11-30 01:15:33', NULL, NULL, NULL),
+(2, NULL, NULL, NULL, '2024-11-30 01:16:11', NULL, NULL, NULL),
+(3, NULL, NULL, NULL, '2024-11-30 01:16:17', NULL, NULL, NULL),
+(4, NULL, NULL, NULL, '2024-11-30 01:17:11', NULL, NULL, NULL),
+(5, NULL, NULL, NULL, '2024-11-30 01:18:23', NULL, NULL, NULL),
+(6, NULL, NULL, NULL, '2024-11-30 01:18:59', NULL, NULL, NULL),
+(7, NULL, NULL, NULL, '2024-11-30 01:24:26', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favemprendedor`
+--
+
+CREATE TABLE `favemprendedor` (
+  `id_favorito` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_negocio` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `favemprendedor`
+--
+
+INSERT INTO `favemprendedor` (`id_favorito`, `id_usuario`, `id_negocio`, `fecha_creacion`) VALUES
+(1, 7, 1, '2024-11-27 22:31:44'),
+(3, 7, 2, '2024-11-27 23:02:04');
 
 -- --------------------------------------------------------
 
@@ -146,15 +231,16 @@ INSERT INTO `favoritos` (`id_favorito`, `id_usuario`, `id_producto`, `fecha_crea
 (2, 1, 5, '2024-11-11 19:16:27'),
 (3, 1, 4, '2024-11-11 19:17:10'),
 (5, 1, 7, '2024-11-11 19:21:09'),
-(6, 1, 10, '2024-11-11 19:21:43'),
 (7, 1, 8, '2024-11-11 19:24:06'),
-(8, 1, 11, '2024-11-11 19:24:09'),
 (9, 1, 5, '2024-11-11 19:52:09'),
 (10, 1, 6, '2024-11-11 20:06:09'),
 (14, 13, 4, '2024-11-18 18:41:24'),
-(19, 11, 11, '2024-11-19 18:06:37'),
 (20, 11, 31, '2024-11-19 18:06:38'),
-(21, 16, 6, '2024-11-19 23:57:20');
+(21, 16, 6, '2024-11-19 23:57:20'),
+(22, 9, 5, '2024-11-26 20:46:27'),
+(23, 7, 5, '2024-11-27 23:02:26'),
+(24, 11, 35, '2024-11-30 00:22:53'),
+(25, 11, 35, '2024-11-30 01:11:18');
 
 -- --------------------------------------------------------
 
@@ -270,7 +356,6 @@ INSERT INTO `interacciones` (`id_interaccion`, `id_perfil`, `id_producto`, `tipo
 (2, 1, NULL, 'View', '2024-11-17 17:08:55'),
 (3, 2, NULL, 'View', '2024-11-17 17:09:16'),
 (4, 1, 5, 'Click', '2024-11-17 17:09:55'),
-(5, 3, 11, 'Click', '2024-11-17 17:10:09'),
 (6, 1, NULL, 'View', '2024-11-18 18:43:06'),
 (7, 4, 30, 'Click', '2024-11-19 18:02:03'),
 (8, 1, 9, 'Click', '2024-11-19 18:02:09'),
@@ -289,17 +374,32 @@ INSERT INTO `interacciones` (`id_interaccion`, `id_perfil`, `id_producto`, `tipo
 (21, 1, NULL, 'View', '2024-11-19 22:53:20'),
 (22, 1, NULL, 'View', '2024-11-19 23:01:01'),
 (23, 1, 6, 'Click', '2024-11-19 23:57:10'),
-(24, 3, 33, 'Click', '2024-11-20 00:00:12'),
 (25, 4, 30, 'Click', '2024-11-20 00:01:26'),
 (26, 1, NULL, 'View', '2024-11-20 00:04:52'),
-(27, 3, 33, 'Click', '2024-11-22 21:40:29'),
 (28, 1, 6, 'Click', '2024-11-22 21:48:13'),
 (29, 1, 6, 'Click', '2024-11-22 21:48:14'),
 (30, 1, NULL, 'View', '2024-11-22 22:04:26'),
 (31, 1, NULL, 'View', '2024-11-26 01:01:28'),
 (32, 1, NULL, 'View', '2024-11-26 01:18:10'),
 (33, 1, NULL, 'View', '2024-11-26 01:28:39'),
-(34, 1, NULL, 'View', '2024-11-26 02:37:23');
+(34, 1, NULL, 'View', '2024-11-26 02:37:23'),
+(35, 1, 6, 'Click', '2024-11-26 19:25:41'),
+(36, 3, NULL, 'View', '2024-11-26 19:31:41'),
+(37, 1, NULL, 'View', '2024-11-26 19:52:08'),
+(38, 3, NULL, 'View', '2024-11-26 20:20:35'),
+(39, 2, NULL, 'View', '2024-11-26 20:22:46'),
+(40, 1, NULL, 'View', '2024-11-26 20:22:53'),
+(41, 1, NULL, 'View', '2024-11-27 21:35:47'),
+(42, 1, NULL, 'View', '2024-11-27 22:31:04'),
+(43, 3, NULL, 'View', '2024-11-27 22:51:11'),
+(44, 3, NULL, 'View', '2024-11-27 22:56:43'),
+(45, 2, NULL, 'View', '2024-11-27 23:00:44'),
+(46, 1, NULL, 'View', '2024-11-29 22:41:28'),
+(47, 3, 35, 'Click', '2024-11-30 00:37:48'),
+(48, 3, 35, 'Click', '2024-11-30 00:46:48'),
+(49, 1, 6, 'Click', '2024-11-30 01:15:09'),
+(50, 3, 35, 'Click', '2024-11-30 01:15:19'),
+(51, 1, 6, 'Click', '2024-11-30 01:24:14');
 
 -- --------------------------------------------------------
 
@@ -328,7 +428,9 @@ INSERT INTO `mensajes` (`id_mensaje`, `id_conversacion`, `id_remitente`, `id_des
 (4, 1, 7, 9, 'hola', 0, '2024-11-26 01:44:54'),
 (5, 1, 7, 9, 'hola', 0, '2024-11-26 01:46:18'),
 (6, 1, 7, 9, 'xd', 0, '2024-11-26 01:49:26'),
-(7, 1, 9, 9, 'hola', 0, '2024-11-26 02:27:13');
+(7, 1, 9, 9, 'hola', 0, '2024-11-26 02:27:13'),
+(8, 1, 7, 9, 'xd', 0, '2024-11-26 19:21:19'),
+(9, 1, 7, 9, 'buenas', 0, '2024-11-26 19:24:11');
 
 -- --------------------------------------------------------
 
@@ -400,7 +502,7 @@ CREATE TABLE `perfil_negocio` (
 --
 
 INSERT INTO `perfil_negocio` (`id_perfil`, `id_usuario`, `id_imagen`, `nombre_negocio`, `descripcion`, `direccion`, `telefono`, `sitioweb_url`, `tipo_perfil`, `id_foro`, `tiempo_creacion`) VALUES
-(1, 9, NULL, 'malumabeiby', NULL, NULL, NULL, NULL, 'Free', NULL, '2024-10-31 20:07:09'),
+(1, 9, NULL, 'malumabeiby', 'el mejor', 'Chuchuncocity 339', '975122121', 'gentecomunicaciones.cl', 'Free', NULL, '2024-10-31 20:07:09'),
 (2, 10, NULL, 'barbershopking', NULL, NULL, NULL, NULL, 'Free', NULL, '2024-10-31 20:30:09'),
 (3, 11, NULL, 'flower', NULL, NULL, NULL, NULL, 'Free', NULL, '2024-11-01 22:09:47'),
 (4, 13, NULL, 'Productos de Aseo', 'Somos un emprendimiento que empezó hace poco a entrar en este mundo, queremos hacer un cambio', 'Calle Falsa 123', '+56988888888', NULL, 'Free', NULL, '2024-11-14 12:17:02'),
@@ -434,8 +536,6 @@ INSERT INTO `productos` (`id_producto`, `id_perfil`, `nombre`, `descripcion`, `i
 (7, 2, 'babershopking', 'cortes de pelo y perfilado de barba en la mejor barberia de rancagua', 5, '10000.00', 0, '2024-10-31 20:36:03'),
 (8, 2, 'insumos barbershop', 'insumos de barberia  los mejores a muy buen precio ', 6, '19990.00', 20, '2024-11-01 16:30:03'),
 (9, 1, 'Poncho de Huaso', 'poncho de huaso echo a mano artesania regional ', 7, '32990.00', 5, '2024-11-01 16:46:40'),
-(10, 3, 'flores ', 'L as flores mas exlcusivas y hermosas de la ciudad ', 8, '15990.00', 22, '2024-11-01 22:13:24'),
-(11, 3, 'rosas', 'rosas rojas para el dia del amor', 9, '10000.00', 25, '2024-11-01 22:18:04'),
 (25, 1, 'sdg', 'gsdgs', NULL, '2442.00', 1, '2024-11-02 19:40:46'),
 (26, 1, 'sdg', 'gsdgs', NULL, '2442.00', 1, '2024-11-02 19:40:47'),
 (27, 1, 'sdg', 'gsdgs', NULL, '2442.00', 1, '2024-11-02 19:41:00'),
@@ -443,7 +543,44 @@ INSERT INTO `productos` (`id_producto`, `id_perfil`, `nombre`, `descripcion`, `i
 (29, 1, 'sdgs', 'sdgs', NULL, '232.00', 1, '2024-11-02 19:42:29'),
 (30, 4, 'Producto 1', 'Descripción del producto 1', 9, '1000.00', 10, '2024-11-18 18:43:06'),
 (31, 4, 'Producto 2', 'Descripción del producto 2', 9, '2000.00', 5, '2024-11-18 18:43:06'),
-(33, 3, 'hola', 'djasjkdajskdjasjdjaskjdjk', 15, '50000.00', 55, '2024-11-19 23:58:38');
+(35, 3, 'claveles', 'mejore claveles', NULL, '2000.00', 48, '2024-11-29 22:59:03');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_imagenes`
+--
+
+CREATE TABLE `producto_imagenes` (
+  `id_imagen` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `url_imagen` varchar(255) NOT NULL,
+  `es_principal` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto_imagenes`
+--
+
+INSERT INTO `producto_imagenes` (`id_imagen`, `id_producto`, `url_imagen`, `es_principal`) VALUES
+(4, 35, 'https://content.elmueble.com/medio/2023/05/23/claveles_8ea9bd65_230523104229_1000x1500.jpg', 0),
+(5, 35, 'https://blog.germigarden.com/wp-content/uploads/2022/04/carnation-chinese-3591757_1920-1024x768.jpg', 0),
+(6, 35, 'https://blog.germigarden.com/wp-content/uploads/2022/04/carnation-chinese-3591757_1920-1024x768.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product_reviews`
+--
+
+CREATE TABLE `product_reviews` (
+  `id_review` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `calificacion` int(11) NOT NULL CHECK (`calificacion` between 1 and 5),
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -521,7 +658,7 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `tipo_usuario`, `email`,
 (6, 'hola', 'user', 'hola@gmail.com', 'hola', NULL, '2024-10-30 03:34:43'),
 (7, 'eric', 'user', 'eric@gmail.com', 'eric', 'https://i.pinimg.com/736x/17/df/b7/17dfb7ccd910388cf96aed0c23173e7b.jpg', '2024-10-30 22:21:00'),
 (8, 'xd', 'emprendedor', 'x@x.x', '123', NULL, '2024-10-31 21:35:24'),
-(9, 'malumabeiby', 'emprendedor', 'm1@gmail.com', '12345', NULL, '2024-10-31 23:07:09'),
+(9, 'malumabeiby', 'emprendedor', 'm1@gmail.com', '12345', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStQgMbIeIJkjUDL6cAuNnzat8IwwFTdKl2ig&s', '2024-10-31 23:07:09'),
 (10, 'barbershopking', 'emprendedor', 'barber@xd.cl', '54321', NULL, '2024-10-31 23:30:09'),
 (11, 'flower', 'emprendedor', 'flower@xd.com', '12345', 'https://img.freepik.com/vector-premium/dibujo-flores-coloridas-colores-arco-iris_761413-3871.jpg', '2024-11-02 01:09:47'),
 (12, 'holaaa', 'user', 'wenas@gmail.com', 'wenas', NULL, '2024-11-02 22:16:23'),
@@ -582,10 +719,26 @@ ALTER TABLE `documento`
   ADD PRIMARY KEY (`id_documento`);
 
 --
+-- Indices de la tabla `entrepreneur_reviews`
+--
+ALTER TABLE `entrepreneur_reviews`
+  ADD PRIMARY KEY (`id_review`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_emprendedor` (`id_emprendedor`);
+
+--
 -- Indices de la tabla `envio`
 --
 ALTER TABLE `envio`
   ADD PRIMARY KEY (`id_envio`);
+
+--
+-- Indices de la tabla `favemprendedor`
+--
+ALTER TABLE `favemprendedor`
+  ADD PRIMARY KEY (`id_favorito`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_negocio` (`id_negocio`);
 
 --
 -- Indices de la tabla `favoritos`
@@ -668,8 +821,22 @@ ALTER TABLE `perfil_negocio`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `profile_id` (`id_perfil`),
-  ADD KEY `id_imagen` (`id_imagen`);
+  ADD KEY `profile_id` (`id_perfil`);
+
+--
+-- Indices de la tabla `producto_imagenes`
+--
+ALTER TABLE `producto_imagenes`
+  ADD PRIMARY KEY (`id_imagen`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `product_reviews`
+--
+ALTER TABLE `product_reviews`
+  ADD PRIMARY KEY (`id_review`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_producto` (`id_producto`);
 
 --
 -- Indices de la tabla `promociones`
@@ -714,37 +881,49 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `conversaciones`
 --
 ALTER TABLE `conversaciones`
-  MODIFY `id_conversacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_conversacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_compra`
 --
 ALTER TABLE `detalle_compra`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `documento`
 --
 ALTER TABLE `documento`
-  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `entrepreneur_reviews`
+--
+ALTER TABLE `entrepreneur_reviews`
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `envio`
 --
 ALTER TABLE `envio`
-  MODIFY `id_envio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_envio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `favemprendedor`
+--
+ALTER TABLE `favemprendedor`
+  MODIFY `id_favorito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `favoritos`
 --
 ALTER TABLE `favoritos`
-  MODIFY `id_favorito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_favorito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `foro`
@@ -774,13 +953,13 @@ ALTER TABLE `imagen_publicacion`
 -- AUTO_INCREMENT de la tabla `interacciones`
 --
 ALTER TABLE `interacciones`
-  MODIFY `id_interaccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id_interaccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
 --
 ALTER TABLE `mensajes`
-  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `metricas`
@@ -804,7 +983,19 @@ ALTER TABLE `perfil_negocio`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT de la tabla `producto_imagenes`
+--
+ALTER TABLE `producto_imagenes`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `product_reviews`
+--
+ALTER TABLE `product_reviews`
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `promociones`
@@ -854,6 +1045,20 @@ ALTER TABLE `conversaciones`
 --
 ALTER TABLE `detalle_compra`
   ADD CONSTRAINT `detalle_compra_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`);
+
+--
+-- Filtros para la tabla `entrepreneur_reviews`
+--
+ALTER TABLE `entrepreneur_reviews`
+  ADD CONSTRAINT `entrepreneur_reviews_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `entrepreneur_reviews_ibfk_2` FOREIGN KEY (`id_emprendedor`) REFERENCES `perfil_negocio` (`id_perfil`);
+
+--
+-- Filtros para la tabla `favemprendedor`
+--
+ALTER TABLE `favemprendedor`
+  ADD CONSTRAINT `favemprendedor_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `favemprendedor_ibfk_2` FOREIGN KEY (`id_negocio`) REFERENCES `perfil_negocio` (`id_perfil`);
 
 --
 -- Filtros para la tabla `favoritos`
@@ -921,6 +1126,19 @@ ALTER TABLE `perfil_negocio`
 ALTER TABLE `productos`
   ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `perfil_negocio` (`id_perfil`),
   ADD CONSTRAINT `productos_ibfk_3` FOREIGN KEY (`id_imagen`) REFERENCES `imagen_publicacion` (`id_imagen`);
+
+--
+-- Filtros para la tabla `producto_imagenes`
+--
+ALTER TABLE `producto_imagenes`
+  ADD CONSTRAINT `producto_imagenes_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `product_reviews`
+--
+ALTER TABLE `product_reviews`
+  ADD CONSTRAINT `product_reviews_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `product_reviews_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
 
 --
 -- Filtros para la tabla `promociones`
